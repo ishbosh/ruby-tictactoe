08f1cd30417c5a4eeb38f19e7f881b8152a39ea2
ishbosh <99460383+ishbosh@users.noreply.github.com>
2022-11-22 10:59:58 -0700

    Refactor win/tie logic
    Changed the way win and tie logic works. Board now has a method to check
    if it is full, and has a method to check the board against the win array
    Game class checks for a winner on the board, and if there is no winner
    then it checks if the board is full.

--- main.rb
+++ main.rb
@@ -4,7 +4,7 @@ module TicTacToe
   class Game
     include DisplayText
 
-    attr_reader :player1, :player2, :board, :players
+    attr_reader :board, :players
     
     def initialize
       @board = Board.new
@@ -18,8 +18,7 @@ module TicTacToe
 
     def restart
       @board = Board.new
-      @player1.turn = false
-      @player2.turn = false
+      players.each {|player| player.turn = false}
       play()
     end
 
@@ -50,80 +49,18 @@ module TicTacToe
       tie = false
       until tie || @winner
         # update player to current player
-        player = player1.turn? ? player1 : player2
+        current_player = players.select {|player| player.turn?}
         #print the board
         board.show()
         #ask the player to take their turn
-        selection_keys = player.take_turn(board)
+        selection_keys = current_player.take_turn(board)
         #ask the board for an update
-        board.update_display(player, selection_keys)
+        board.update_display(current_player, selection_keys)
         #check for winner or tie
-        @winner = check_for_winner()
-        if @winner == 'tie' then tie = true end
+        @winner = board.winner?(current_player)
+        @tie = board.full?
         # change the turns (called on both players to swap each one's turn value)
-        player1.change_turn
-        player2.change_turn
-      end
-    end
-
-    def check_for_winner
-      row_keys = [:top, :mid, :bot]
-      row_grid = row_keys.map do |row|
-        board.moves[row].map {|k, v| v}
-      end
-      # convert rows to clumns
-      col_grid = row_grid.transpose
-
-      #check rows for winner
-      winner = count_marks(row_grid)
-      if winner then return winner end
-      #check columns for winner
-      winner = count_marks(col_grid)
-      if winner then return winner end
-      #check diagonals for winner
-      winner = count_diagonals()
-      if winner then return winner end
-      unless row_grid.flatten.include?(nil)
-        return 'tie'
-      end
-    end
-
-    def count_marks(grid)
-      winner = nil
-      x_count = grid.map {|row| row.count('X')}
-      o_count = grid.map {|row| row.count('O')}
-
-      results = {X: x_count, O: o_count}
-      results.each do |mark, counts|
-        counts.each do |count|
-          if count == 3
-            winner = mark.to_s
-          end
-        end
-        winner
-      end
-      winner
-    end
-
-    def count_diagonals
-      tl_to_br = 
-        [
-          board.moves[:top][:left], 
-          board.moves[:mid][:mid], 
-          board.moves[:bot][:right]
-        ]
-      bl_to_tr = 
-        [
-          board.moves[:bot][:left],
-          board.moves[:mid][:mid],
-          board.moves[:bot][:right]
-        ]
-      x_count = [tl_to_br.count('X'), bl_to_tr.count('X')]
-      o_count = [tl_to_br.count('O'), bl_to_tr.count('O')]
-      if x_count[0] == 3 || x_count[1] == 3
-        return 'X'
-      elsif o_count[0] == 3 || o_count[1] == 3
-        return 'O'
+        players.each {|player| player.change_turn}
       end
     end
 
@@ -145,12 +82,12 @@ module TicTacToe
     public
 
   end
-
+########### - NEW CLASS:
   class Board
     include DisplayText
     attr_reader :board
 
-    WIN_INDEXES = [[0,1,2], [3,4,5], [6,7,8], [0,3,6],
+    WIN_LINES = [[0,1,2], [3,4,5], [6,7,8], [0,3,6],
                    [1,4,8], [2,5,8], [0,4,8], [2,4,6]].freeze
 
     MOVE_INDEX_HASH = {top: 0, mid: 1, bot: 2, left: 0, mid: 1, right: 2}.freeze
@@ -178,9 +115,7 @@ module TicTacToe
       unless input.include?('-')
         return false
       end
-      keys = input.split('-')
-      keys[0] = keys[0].to_sym #eg.  :top
-      keys[1] = keys[1].to_sym #eg.  :left
+      keys = convert_to_keys(input)
       if MOVE_INDEX_HASH.include?(keys[0]) && MOVE_INDEX_HASH.include?(keys[1])
         if board[MOVE_INDEX_HASH[keys[0]]][MOVE_INDEX_HASH[keys[1]]] == '  '
           return true
@@ -188,6 +123,19 @@ module TicTacToe
       else
         return false
       end
+    end
+
+    def win_move?(current_player)
+      flat_board = board.flatten
+      WIN_LINES.any? do |line|
+        [flat_board[line[0]], 
+         flat_board[line[1]], 
+         flat_board[line[2]]].uniq.eql?([" #{current_player.mark} "])
+    end
+
+    def full?
+      open_space = board.flatten.select {|space| space == '   '}
+      open_space.empty?
     end
 
     private
@@ -202,6 +150,12 @@ module TicTacToe
         return row_index, col_index
     end
 
+    def convert_to_keys(input)
+      array = input.split('-')
+      keys = [array[0].to_sym, array[1].to_sym]
+      keys
+    end
+
     def update_display(player, selection_keys)
       # Get the indexes of the selection. Row then Col
       index_array = convert_to_index(selection_keys)
@@ -211,7 +165,7 @@ module TicTacToe
     end
     
   end
-
+  ########## - NEW CLASS:
   class Player
     include DisplayText
 
@@ -252,9 +206,8 @@ module TicTacToe
 
     private
 
-    def convert_to_keys(validated_input)
-      # convert the input to the key equivalent
-      array = validated_input.split('-')
+    def convert_to_keys(input)
+      array = input.split('-')
       keys = [array[0].to_sym, array[1].to_sym]
       keys
     end
